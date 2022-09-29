9342bd2c5a3376b60606d76f175ece72346aff60
IsabelSilvera2733 <yosola1227@gmail.com>
2022-09-29 10:12:00 -0700

    P6 T9 t1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
