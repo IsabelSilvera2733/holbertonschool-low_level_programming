c4c86cb16cb95a93a295fda452afa386c0cbc8ae
IsabelSilvera2733 <yosola1227@gmail.com>
2022-10-06 07:25:35 -0700

    P11 T9 t5

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table -  function that prints the 9 times table
+ */
+void times_table(void)
+{
+	int dividend;
+	int divider;
+	int resultado;
+	char *sep = " ";
+	char *sep1 = ",";
+
+	for (divider = 0; divider < 10; divider++)	/*x-axis*/
+	{
+		for (dividend = 0; dividend < 10; dividend++)	/*y-axis*/
+		{
+			resultado = divider * dividend;		/*result the x * y*/
+			if (resultado > 9)
+			{
+				_putchar((resultado / 10) + '0');	/*prints the first digit in ASCII*/
+				_putchar((resultado % 10) + '0');	/*prints last digit in ASCII value*/
+			}
+			else if (dividend != 0)		/*from 1 to 8 print the separator plus the result*/
+			{
+				_putchar(*sep);
+				_putchar(resultado + '0');
+			}
+			else
+				_putchar(resultado + '0');
+			if (dividend != 9)
+			{
+				_putchar(*sep1);
+				_putchar(*sep);
+			}
+		}
+		_putchar('\n');	/*line break between each result on the x-axis*/
+	}
+}
