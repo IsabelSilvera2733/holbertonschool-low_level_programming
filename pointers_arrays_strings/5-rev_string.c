2818b56c25dc81d41c543bc6096bf32a7142a33b
IsabelSilvera2733 <yosola1227@gmail.com>
2022-10-12 05:43:32 -0700

    P14 T6 t1

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int i, m;
+	char aux;
+
+	i = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	}
